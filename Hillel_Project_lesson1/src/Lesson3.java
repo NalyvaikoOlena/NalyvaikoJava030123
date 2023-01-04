79b750cbbbba226722e36e3c665ecb57011d886f
Olena2023 <olenanalivayko@gmail.com>
2023-01-04 13:38:51 +0200

    refactoring

--- Hillel_Project_lesson1/src/Lesson3.java
+++ Hillel_Project_lesson1/src/Lesson3.java
@@ -1,14 +1,14 @@
 public class Lesson3 {
     public static void main (String[] args){
-        int lenght = 5;
-        int widht = 7;
+        int length = 5;
+        int width = 7;
         int height = 4;
 
-        int volume = lenght * widht * height;
-        int lenghtTotal = 4 * (lenght + widht + height);
+        int volume = length * width * height;
+        int lengthTotal = 4 * (length + width + height);
 
         System.out.println("Volume of the parallelepiped = " +volume);
-        System.out.println("The perimetr of the parallelepiped = " +lenghtTotal);
+        System.out.println("The perimetr of the parallelepiped = " +lengthTotal);
 
     }
 }
