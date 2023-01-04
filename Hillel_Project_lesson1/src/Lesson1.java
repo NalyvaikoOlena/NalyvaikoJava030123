ded0133d506aa62bb8330eb9745d2208b15024eb
Olena2023 <olenanalivayko@gmail.com>
2023-01-04 13:22:25 +0200

    first message

---
+++ Hillel_Project_lesson1/src/Lesson1.java
@@ -0,0 +1,10 @@
+public class Lesson1 {
+    public static void main (String[] args) {
+
+        double latitude=54.43952;
+        System.out.println("latitude="+latitude);
+        double longitude=18.54309;
+        System.out.println("longitude="+longitude);
+    }
+}
+
---
+++ Hillel_Project_lesson1/src/Lesson3.java
@@ -0,0 +1,14 @@
+public class Lesson3 {
+    public static void main (String[] args){
+        int lenght = 5;
+        int widht = 7;
+        int height = 4;
+
+        int volume = lenght * widht * height;
+        int lenghtTotal = 4 * (lenght + widht + height);
+
+        System.out.println("Volume of the parallelepiped = " +volume);
+        System.out.println("The perimetr of the parallelepiped = " +lenghtTotal);
+
+    }
+}
